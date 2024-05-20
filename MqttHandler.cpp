8fbee69a39bc9bdc71d7b72680398c5228c6b26d
pawelzim <pawel.zimolag02@gmail.com>
2024-05-20 23:02:48 +0200

    Zmiany: dodany POST Request, dodane zmienne do wysyłki, zrobione kilka poprawek

--- MqttHandler.cpp
+++ MqttHandler.cpp
@@ -1,42 +1,46 @@
 #include "MqttHandler.hpp"
 
-MqttHandler::MqttHandler(const char* Ssid, const char* Password, const char* Mqtt_server, const char* Mqtt_user, const char* Mqtt_password) 
-    : ssid(Ssid), password(Password), mqtt_server(Mqtt_server), mqtt_user(Mqtt_user), mqtt_password(Mqtt_password), client(espClient) {}
+MqttHandler::MqttHandler(const char* DeviceName, const char* Ssid, const char* Password, const char* Mqtt_server, const char* Mqtt_user, const char* Mqtt_password) 
+    : deviceName(DeviceName), ssid(Ssid), password(Password), mqtt_server(Mqtt_server), mqtt_user(Mqtt_user), mqtt_password(Mqtt_password), client(espClient) {}
 
 void MqttHandler::begin() {
     Serial.begin(115200);
     pinMode(2, OUTPUT);
     setupWifi();
+    sendPostRequest();
+    setupMqtt();
     setupTime();
-    client.setServer(mqtt_server, 1883);
-    client.connect("ESP32Client", mqtt_user, mqtt_password);
 }
 
-void MqttHandler::handle() {
-    if (!client.connected()) {
-        digitalWrite(2, LOW);
-        reconnect();
-    }
-    digitalWrite(2, HIGH);
+void MqttHandler::handle(int speed, float latitude, float longitude, 
+                         const char* direction, bool isIgnitionOn, bool isDriving, 
+                         bool isDrivingNeutral, float fuelLevel, float mileage, 
+                         const char* diagnosticCodes) {
+    checkConnectionStatus();
     client.loop();
 
-    StaticJsonDocument<200> doc;
-    doc["carId"] = "QWE123123RTY";
-    doc["isTurnedOn"] = true;
-    doc["velocity"] = tempValue;
-    doc["fuelLevel"] = tempValue / 2;
+    StaticJsonDocument<512> doc;
+    JsonArray location = doc.createNestedArray("location");
+    doc["speed"] = speed;
+    location.add(latitude);
+    location.add(longitude);
+    doc["direction"] = direction;
+    doc["isIgnitionOn"] = isIgnitionOn;
+    doc["isDriving"] = isDriving;
+    doc["isDrivingNeutral"] = isDrivingNeutral;
+    doc["fuelLevel"] = fuelLevel;
+    doc["mileage"] = mileage;
+    doc["diagnosticCodes"] = diagnosticCodes;
     doc["time"] = getTime();
     doc["date"] = getDate();
 
     char jsonBuffer[512];
     serializeJson(doc, jsonBuffer);
-    client.publish("topic/testing/qwerty", jsonBuffer);
-    Serial.println("Published topic");
+    client.publish(deviceName, jsonBuffer);
+    Serial.print("Published to topic: '");
+    Serial.print(deviceName);
+    Serial.println("'");
 
-    Serial.print("WiFi status: ");
-    Serial.println(WiFi.status());
-
-    tempValue += 10.5;
     delay(1000);
 }
 
@@ -57,12 +61,125 @@ void MqttHandler::setupWifi() {
 
     Serial.println("");
     Serial.print("Connected to WiFi: ");
-    Serial.println(ssid);
-    Serial.println("IP address: ");
+    Serial.print(ssid);
+    Serial.print(" | IP address: ");
     Serial.println(WiFi.localIP());
 }
 
-void MqttHandler::reconnect() {
+void MqttHandler::sendPostRequest() {
+    WiFiClientSecure clientPostRequest;
+    clientPostRequest.setInsecure();
+
+    HTTPClient httpsPostRequest;
+    String serverName = "https://localhost:7240/devices/";
+    String url = serverName + deviceName;
+
+    while (true) {
+        if (httpsPostRequest.begin(clientPostRequest, url)) {
+            httpsPostRequest.addHeader("Content-Type", "application/json");
+
+            int httpResponseCode = httpsPostRequest.POST("");
+
+            if (httpResponseCode > 0) {
+                String response = httpsPostRequest.getString();
+                Serial.println(httpResponseCode);
+                Serial.println(response);
+                if (httpResponseCode == 200) {
+                    break;
+                }
+            } else {
+                Serial.print("Error sending POST: ");
+                Serial.println(httpResponseCode);
+            }
+
+            httpsPostRequest.end();
+        } else {
+            Serial.println("Unable to connect");
+        }
+
+        delay(3000);
+    }
+}
+
+
+// void MqttHandler::sendPostRequest() {
+//     WiFiClientSecure clientPostRequest;
+//     clientPostRequest.setInsecure();
+
+//     HTTPClient httpsPostRequest;
+//     String serverName = "https://localhost:7240/devices/";
+//     String url = serverName + deviceName;
+
+//     if (httpsPostRequest.begin(clientPostRequest, url)) {
+//         httpsPostRequest.addHeader("Content-Type", "application/json");
+
+//         StaticJsonDocument<200> doc;
+//         doc["status"] = "started";
+//         doc["deviceName"] = deviceName;
+
+//         String requestBody;
+//         serializeJson(doc, requestBody);
+
+//         int httpResponseCode = httpsPostRequest.POST(requestBody);
+
+//         if (httpResponseCode > 0) {
+//             String response = httpsPostRequest.getString();
+//             Serial.println(httpResponseCode);
+//             Serial.println(response);
+//         } else {
+//             Serial.print("Error sending POST: ");
+//             Serial.println(httpResponseCode);
+//         }
+
+//         httpsPostRequest.end();
+//     } else {
+//         Serial.println("Unable to connect");
+//     }
+// }
+
+void MqttHandler::setupMqtt() {
+    client.setServer(mqtt_server, 1883);
+    client.connect("ESP32Client", mqtt_user, mqtt_password);
+    if (client.connected()) {
+        Serial.print("Connected to MQTT Broker ");
+        Serial.println(mqtt_server);
+    }
+    else {
+        reconnect_mqtt();
+    }
+}
+
+void MqttHandler::setupTime() {
+    configTime(gmtOffset_sec, daylightOffset_sec, "pool.ntp.org");
+    struct tm timeinfo;
+    if (!getLocalTime(&timeinfo)) {
+        Serial.println("Failed to obtain NTP time.");
+    } else {
+        Serial.println("NTP time set successfully");
+        Serial.println("");
+    }
+}
+
+void MqttHandler::reconnect_wifi() {
+    Serial.println("WiFi disconnected. Attempting to reconnect...");
+    WiFi.begin(ssid, password);
+    unsigned long startTime = millis();
+    while (WiFi.status() != WL_CONNECTED && millis() - startTime < 10000) {
+        delay(500);
+        Serial.println("Attempting to reconnect Wifi");
+    }
+    if (WiFi.status() == WL_CONNECTED) {
+        Serial.print("Reconnected to WiFi: ");
+        Serial.println(ssid);
+        Serial.print("IP address: ");
+        Serial.println(WiFi.localIP());
+        Serial.println("");
+    } else {
+        Serial.println("Failed to reconnect to WiFi");
+    }
+}
+
+void MqttHandler::reconnect_mqtt() {
     while (!client.connected()) {
         Serial.print("Attempting MQTT connection to: ");
         Serial.println(mqtt_server);
@@ -78,14 +195,15 @@ void MqttHandler::reconnect() {
     }
 }
 
-void MqttHandler::setupTime() {
-    configTime(gmtOffset_sec, daylightOffset_sec, "pool.ntp.org");
-    struct tm timeinfo;
-    if (!getLocalTime(&timeinfo)) {
-        Serial.println("Failed to obtain NTP time.");
-    } else {
-        Serial.println("NTP time set successfully");
+void MqttHandler::checkConnectionStatus() {
+    if (WiFi.status() != WL_CONNECTED) {
+        reconnect_wifi();
     }
+    if (!client.connected()) {
+        digitalWrite(2, LOW);
+        reconnect_mqtt();
+    }
+    digitalWrite(2, HIGH);
 }
 
 String MqttHandler::getTime() {
--- Projekt_zespolowy.ino
+++ Projekt_zespolowy.ino
@@ -1,16 +1,28 @@
 #include "MqttHandler.hpp"
 
+const char* serverName = "https://localhost:7240/devices/";
+const char* deviceName = "deviceId123";
+
+const char* ssid = "Frida";
+const char* password = "Frida27/72";
+
+const char* mqtt_server = "192.168.0.183";
+const char* mqtt_login = "";
+const char* mqtt_password = "";
+
 // mqttHandler(ssid, password, mqtt_server, mqtt_user, mqtt_password)
 //                                          ^puste ""  ^puste ""
-MqttHandler mqttHandler(ssid, password, mqtt_server, mqtt_user, mqtt_password);
+MqttHandler mqttHandler(deviceName, ssid, password, mqtt_server, mqtt_login, mqtt_password);
+double tempValue = 150;
 
 void setup() {
     mqttHandler.begin();
 }
 
 void loop() {
+    // mqttHandler.handle(speed, latitude, longitude, direction, 
+    //                    isIgnitionOn, isDriving, isDrivingNeutral,
+    //                    fuelLevel, mileage, diagnosticCodes);
     mqttHandler.handle();
+    tempValue += 15;
 }
-
-
-
